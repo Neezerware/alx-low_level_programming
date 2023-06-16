b3e6b8e0008a294322d420b29e6d52bb6e8090f6
Sunday Ojo <sunday.ojo90@yahoo.com>
2023-06-16 09:50:58 -0700

    Print all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints all possible combinations of two two-digit
+ * numbers ranging from 0 to 99 and number 1 printed as 01
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
