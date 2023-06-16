de729e854ea82d9be69f31ccc26903a1c3caa77e
Sunday Ojo <sunday.ojo90@yahoo.com>
2023-06-16 08:59:26 -0700

    Print alphebets in lowercase and uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase and then in uppercase.
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	char letters[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(letters[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
