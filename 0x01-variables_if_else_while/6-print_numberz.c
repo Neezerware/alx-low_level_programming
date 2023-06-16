ba911c289d17058d6ce984a67eeae1d54bf96bae
Sunday Ojo <sunday.ojo90@yahoo.com>
2023-06-16 09:10:18 -0700

    Print single digit numbers of base 10 from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers of Base 10 starting from 0.
+ * Return:0 (Successful)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
