1d7cf5648e720548e17f858d3001f34ea9f41f07
Sunday Ojo <sunday.ojo90@yahoo.com>
2023-06-20 12:22:16 -0700

    Print all alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		_putchar(alp);
+
+	_putchar('\n');
+}
+
