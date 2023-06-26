9b12235d319b42750116829cbcd73e656ce6be95
Sunday Ojo <sunday.ojo90@yahoo.com>
2023-06-26 13:51:01 -0700

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,7 @@ void puts_half(char *str)
 	if ((slen % 2) == 1)
 		n = ((slen + 1) / 2);
 
-	for (i = n; str[i] != '\0'; a++)
+	for (i = n; str[i] != '\0'; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
