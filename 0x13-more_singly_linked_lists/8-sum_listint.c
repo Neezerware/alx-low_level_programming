800808c4e1be0531a14721f74fc5c6e869e27a5a
Sunday Ojo <sunday.ojo90@yahoo.com>
2023-08-01 11:22:38 -0700

    8-sum_listint

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - a function that returns the sum of all
+ * the data (n) of a listint_t linked list
+ * @head: first node in the linked list
+ *
+ * Return: sum of all data
+ */
+int sum_listint(listint_t *head)
+{
+	if (head == NULL)
+		return (0);
+
+	int sum = 0;
+	listint_t *temp = head;
+
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
