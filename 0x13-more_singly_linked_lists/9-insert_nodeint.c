b039f0aa8b095797de5d2e2d28a61914a3e6fdca
Sunday Ojo <sunday.ojo90@yahoo.com>
2023-08-01 12:59:28 -0700

    9-insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,44 +3,43 @@
 /**
  * insert_nodeint_at_index - a function that inserts
  * a new node at a given position
- * @head: pointer to the first node in the list
- * @idx: index of the list where the node should be added
- * @n: new node to be added
+ * @head: the first node in the list
+ * @idx: index of the list where the new node should be added
+ * @n: data to insert in the new node
  *
- * Return: pointer to the new node, or NULL
+ * Return: address of the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *temp = *head;
 	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
 
-	if (!new_node || !head)
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = NULL;
+	new->n = n;
+	new->next = NULL;
 
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
 
 	for (i = 0; temp && i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
-			new_node->next = temp->next;
-			temp->next = new_node;
-			return (new_node);
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
 		}
 		else
-		{
-			temp = temp->next
-		}
+			temp = temp->next;
 	}
-	free(new_node);
+
 	return (NULL);
 }
