fdad504eddb50c17943b1c057e7ba61fa1b0212a
Sunday Ojo <sunday.ojo90@yahoo.com>
2023-08-01 12:34:37 -0700

    9-insert_nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,12 +11,9 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	if (head == NULL)
-		return (NULL);
-
 	listint_t *new_node = malloc(sizeof(listint_t));
 
-	if (!new_node)
+	if (!new_node || !head)
 		return (NULL);
 
 	new_node->n = n;
