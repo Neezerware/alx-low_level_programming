f7cd2b5a818b5ea1c25d4a36152a79d94bd61d21
Sunday Ojo <sunday.ojo90@yahoo.com>
2023-08-03 18:35:39 -0700

    1-print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - a function that prints the binary representation of a number
+ * @n: binary result
+ */
+print_binary(unsigned long int n)
+{
+	int counter = 0;
+	unsigned long int value;
+	int i;
+
+	for (i = 63; i >= 0; i--)
+	{
+		value = n >> i;
+
+		if (value & 1)
+		{
+			_putchar('1');
+			counter++;
+		}
+		else if (counter)
+			_putchar('0');
+	}
+	if (!counter)
+		_putchar('0');
+}
